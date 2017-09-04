16e2cca6f67c2f629c7e81f84f9f7ef65d6598e9
Josef Richter <josef.richter@me.com>
2017-09-04 09:45:42 +0200

    gulp settings updated, basic styling test

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     // gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch(['assets/sass/*.scss'], ['sass']);
+    gulp.watch(['assets/sass/**/*.scss'], ['sass']);
     // gulp.watch("src/*.html").on('change', browserSync.reload);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
