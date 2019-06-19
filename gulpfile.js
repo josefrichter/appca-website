caaf2872b2da179bf4c9a65acf6469dc8c2f1fef
Josef Richter <josef.richter@me.com>
2019-06-19 21:44:39 +0200

    Revert "heroku doesnt support http2 yet, so what am I doing..."
    This reverts commit 1362316cbf42275d3089639f99068dd34918f470.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass        = require('gulp-sass');
 var sourcemaps  = require('gulp-sourcemaps');
 var connect     = require('gulp-connect');
 var nunjucksRender = require('gulp-nunjucks-render');
-// var http2 = require('http2');
+var http2 = require('http2');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -58,9 +58,8 @@ gulp.task('build', function() {
   connect.server({
     root: ".",
     port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-    // livereload: false,
-    // https: true
+    livereload: false,
+    https: true
   });
 });
 
