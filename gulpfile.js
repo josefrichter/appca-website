864d1a2742580cedc574d5f255c0bfb7f7671510
Josef Richter <josef.richter@wilmersrespondek.com>
2021-02-12 10:17:47 +0100

    recompiled sass

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,11 @@ var nunjucksRender = require('gulp-nunjucks-render');
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     // return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-    return gulp.src(['assets/sass/*.scss'])
+    return gulp.src(['public/sass/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("assets/css"))
+        .pipe(gulp.dest("public/css"))
         .pipe(browserSync.stream());
 });
 
@@ -26,16 +26,16 @@ gulp.task('js', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('nunjucks', function() {
-  // Gets .html and .nunjucks files in pages
-  return gulp.src('pages/**/*.+(html|njk)')
-  // Renders template with nunjucks
-  .pipe(nunjucksRender({
-      path: ['templates']
-    }))
-  // output files in app folder
-  .pipe(gulp.dest('.'))
-});
+// gulp.task('nunjucks', function() {
+//   // Gets .html and .nunjucks files in pages
+//   return gulp.src('pages/**/*.+(html|njk)')
+//   // Renders template with nunjucks
+//   .pipe(nunjucksRender({
+//       path: ['templates']
+//     }))
+//   // output files in app folder
+//   .pipe(gulp.dest('.'))
+// });
 
 // Static Server + watching scss/html files
 gulp.task('localserve', gulp.series('sass'), function() {
@@ -45,7 +45,7 @@ gulp.task('localserve', gulp.series('sass'), function() {
     });
 
     // gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch(['assets/sass/**/*.scss'], ['sass']);
+    gulp.watch(['public/sass/**/*.scss'], ['sass']);
     gulp.watch(['pages/**/*.+(html|njk)', 'templates/**/*.+(html|njk)'], ['nunjucks']);
     // gulp.watch("src/*.html").on('change', browserSync.reload);
     gulp.watch("*.html").on('change', browserSync.reload);
